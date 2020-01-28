723155b778c528516094f3ddd002f23848394219
jhuthmacher <jhuthmacher@gmail.com>
2020-01-28 14:13:20 -0800

    functionality for adding reviews and helpful count in db

--- db/dbHelpers.js
+++ db/dbHelpers.js
@@ -1,9 +1,28 @@
 const Reviews = require('./model');
 
 const helpers = {
-    get: () =>  Reviews.find({}) ,
-    post: (item) =>  Reviews.create(item), // have item come in as object w proper ids
-    update: (_id, item) => Reviews.findByIdAndUpdate(_id, item) ,
+    getAll: () =>  Reviews.find({}) ,
+    getOne: (productID) => Reviews.find({productID}),
+    // post: (item) =>  Reviews.create(item), // have item come in as object w proper ids
+    addReview: (productID, item) => {
+      Reviews.find({productID})
+        .then(product => {
+          var reviews = product.reviews;
+          reviews.push(item);
+          return Reviews.findOneAndUpdate({productID}, reviews)
+        })
+    },
+    addHelpful: (productID, _id, item) => {
+      Reviews.find({productID})
+        .then(product => {
+          var reviews = product.reviews;
+          var curIdx = reviews.findIndex((review) => review._id === _id);
+          var currentReview = reviews[curIdx];
+          currentReview.helpful[item] = currentReview.helpful[item] + 1;
+          reviews[curIdx] = currentReview;
+          return Reviews.findOneAndUpdate({productID}, reviews)
+        })
+    },
     delete: (_id) => Reviews.findByIdAndDelete(_id) 
 }
 
