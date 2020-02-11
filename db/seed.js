4c9449d401e1adc9b49d8cdf6604efbe4560d00d
jhuthmacher <jhuthmacher@gmail.com>
2020-02-10 16:18:13 -0800

    add photos, new seed

--- db/seed.js
+++ db/seed.js
@@ -1,25 +1,25 @@
 const faker = require('faker/index');
 const Reviews = require('./model');
+const allPhotos = require('./photos');
+const _ = require('lodash');
 
 Reviews.remove({})
-  .then(() => {console.log('old db clear, seeding new db...')})
+  .then(() => { console.log('old db clear, seeding new db...') })
   .then(() => {
     for (var i = 0; i < 100; i++) {
-
-      var numReviews = faker.random.number({ min: 15, max: 50 });
-    
       var reviews = [];
+      var numReviews = faker.random.number({ min: 15, max: 50 });
 
-      var photos = [
-        '../rei-photos/1.jpg', 
-        '../rei-photos/2.jpg', 
-        '../rei-photos/3.jpg', 
-        '../rei-photos/4.jpg', 
-        '../rei-photos/5.jpg', 
-        '../rei-photos/6.jpg' 
-      ]
-    
       for (var j = 0; j < numReviews; j++) {
+        var numPhotos = faker.random.number({ min: 0, max: 6 });
+        var rawPhotos = [];
+
+        for (var k = 0; k < numPhotos; k++) {
+          rawPhotos.push(allPhotos[faker.random.number({ min: 0, max: 24 })])
+        }
+
+        var photos = _.uniq(rawPhotos);
+
         var review = {
           username: faker.internet.userName(),
           title: faker.company.catchPhrase(),
@@ -35,18 +35,18 @@ Reviews.remove({})
         }
         reviews.push(review);
       }
-    
+
       let product = {
         productID: i,
         reviews: reviews
       }
-    
+
       Reviews.create(product);
       console.log('product ', i, ' created')
     }
 
   })
-  .then(() => { 
+  .then(() => {
     console.log('db seeded')
   })
   .catch(err => console.log(err))
